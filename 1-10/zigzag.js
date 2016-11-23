b1999b97f502a49efdd7e71512dcc550d3d518fc
ziofat <printlts@gmail.com>
2016-11-23 21:11:25 +1100

    fix style

--- 1-10/zigzag.js
+++ 1-10/zigzag.js
@@ -4,25 +4,25 @@
  * @return {string}
  */
 var convert = function(s, numRows) {
-	if (numRows == 1) {
-		return s;
-	}
+    if (numRows == 1) {
+        return s;
+    }
     var sarr = s.split("");
-	var length = s.length;
-	var result = [];
-	var pointer = 0;
-	var flag = 1;
-	for (i = 0; i < length ; i++) {
-		if(!result[pointer]) {
-			result[pointer] = '';
-		}
-		result[pointer] += sarr[i];
-		if (pointer === 0 && flag == -1) {
-			flag = 1;
-		} else if (pointer == numRows - 1 && flag == 1) {
-			flag = -1;
-		}
-		pointer += flag;
-	}
-	return result.join('');
+    var length = s.length;
+    var result = [];
+    var pointer = 0;
+    var flag = 1;
+    for (i = 0; i < length ; i++) {
+        if(!result[pointer]) {
+            result[pointer] = '';
+        }
+        result[pointer] += sarr[i];
+        if (pointer === 0 && flag == -1) {
+            flag = 1;
+        } else if (pointer == numRows - 1 && flag == 1) {
+            flag = -1;
+        }
+        pointer += flag;
+    }
+    return result.join('');
 };
