aaeef61eaa5004cc57b67599f5b379b8b7fb2378
ziofat <printlts@gmail.com>
2016-11-23 19:59:33 +1100

    style: use cdns

--- 1-10/addTwoNumbers.js
+++ 1-10/addTwoNumbers.js
@@ -12,25 +12,25 @@
  */
 var addTwoNumbers = function(l1, l2) {
     var lr = new ListNode(0);
-	var lc = lr;
-	while(l1 || l2) {
-		if (!l1) {
-			l1 = new ListNode(0);
-		}
-		if (!l2) {
-			l2 = new ListNode(0);
-		}
-		if (!lc.next) {
-			lc.next = new ListNode(0);
-		}
-		lc = lc.next;
-		lc.val += l1.val + l2.val;
-		if(lc.val >= 10){
-			lc.val-= 10;
-			lc.next = new ListNode(1);
-		}
-		l1 = l1.next;
-		l2 = l2.next;
-	}
-	return lr.next;
+    var lc = lr;
+    while(l1 || l2) {
+        if (!l1) {
+            l1 = new ListNode(0);
+        }
+        if (!l2) {
+            l2 = new ListNode(0);
+        }
+        if (!lc.next) {
+            lc.next = new ListNode(0);
+        }
+        lc = lc.next;
+        lc.val += l1.val + l2.val;
+        if(lc.val >= 10){
+            lc.val-= 10;
+            lc.next = new ListNode(1);
+        }
+        l1 = l1.next;
+        l2 = l2.next;
+    }
+    return lr.next;
 };
